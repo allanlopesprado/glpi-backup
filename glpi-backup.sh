175d83a67b98c79e0212d7bb6138eaa2b45bb04c
Allan Lopes Prado <allanlopesprado@hotmail.com>
2025-12-10 02:47:33 -0300

    Update script version from 3.1.0 to 1.2.0
    Signed-off-by: Allan Lopes Prado <allanlopesprado@hotmail.com>

--- glpi-backup.sh
+++ glpi-backup.sh
@@ -2,7 +2,7 @@
 
 # -------------------------------------------------------------------------
 # @Name: glpi-backup.sh
-# @Version: 3.1.0 (Enterprise Edition)
+# @Version: 1.2.0
 # @Date: 2024-08-08
 # @Author: Allan Lopes Prado
 # @License: GNU General Public License v2.0
